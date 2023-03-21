637e51ae86d5245e653ba35560c9d9960af23825
TechSulai786 <techsulai786@outlook.com>
2023-03-21 13:58:07 +0530

    updated

--- Access String.cpp
+++ Access String.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 int main()
 {
- 
+
     string myString = "Jech Sulai";
-    cout << myString[0]<<endl;
-    cout << myString[1]<<endl;
+    cout << myString[0] << endl;
+    cout << myString[1] << endl;
     myString[0] = 'T';
     cout << myString;
     return 0;
 }
-
-
